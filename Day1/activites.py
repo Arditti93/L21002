e219588c42f805b3cffc727b354af99442b52d4b
unknown <you@example.com>
2025-02-19 09:08:48 +0000

    added print statments

--- Day1/activites.py
+++ Day1/activites.py
@@ -3,34 +3,34 @@ name = "Alex"
 age = 31
 fav_colour = "Green"
 
-#print(f"My name is {name} and my age is {age} and my favourite colour is {fav_colour}") 
+print(f"My name is {name} and my age is {age} and my favourite colour is {fav_colour}") 
 
 # 2
 breakfast = "Toast"
 lunch = "Leftover Pasta"
 dinner = "No idea!"
-#print(f"Today I had {breakfast} for breakfast and for Lunch I had {lunch}. For dinner I have {dinner}")
+print(f"Today I had {breakfast} for breakfast and for Lunch I had {lunch}. For dinner I have {dinner}")
 
 breakfast = "bagle"
 lunch = "Sandwich"
 dinner = "curry"
-#print(f"Today I had {breakfast} for breakfast and for Lunch I had {lunch}. For dinner I have {dinner}")
+print(f"Today I had {breakfast} for breakfast and for Lunch I had {lunch}. For dinner I have {dinner}")
 
 
 # 3
-# print("    |       |  ")
-# print("    |       |  ")
-# print("    |       |  ")
-# print("-----------------")
-# print("    |       |  ")
-# print("    |       |  ")
-# print("    |       |  ")
-# print("-----------------")
-# print("    |       |  ")
-# print("    |       |  ")
-# print("    |       |  ")
+print("    |       |  ")
+print("    |       |  ")
+print("    |       |  ")
+print("-----------------")
+print("    |       |  ")
+print("    |       |  ")
+print("    |       |  ")
+print("-----------------")
+print("    |       |  ")
+print("    |       |  ")
+print("    |       |  ")
 
-# # 4
+# 4
 space1 = " "
 space2 = " "
 space3 = "o"
