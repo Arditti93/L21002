c4816b08f99fa95f8cb1c5e1dc61406a0835fb7e
unknown <you@example.com>
2025-02-19 10:05:51 +0000

    added number calulator

---
+++ Day1/number-inputs.py
@@ -0,0 +1,15 @@
+num_1 = input('Enter number 1: ')
+num_2 = input('Enter number 2: ')
+# +
+print(int(num_1) + int(num_2))
+# -
+print(int(num_1) - int(num_2))
+# / 
+print(int(num_1) / int(num_2)) 
+#*
+print(int(num_1) * int(num_2))
+
+
+
+# name = input('Enter your name: ')
+# print(f"Hello {name}")
