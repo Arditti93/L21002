05c836d69a3bb3a2083271bb117d42d800f522a0
unknown <you@example.com>
2025-02-19 09:42:03 +0000

    added operators

---
+++ Day1/operator-activities.py
@@ -0,0 +1,34 @@
+# 1 
+# Arithmetic Operators
+num_1 = 6
+num_2 = 4
+# +
+print(num_1 + num_2)
+# # -
+print(num_1 - num_2)
+
+# # / 
+print(num_1 / num_2) 
+
+print(num_1 * num_2)
+
+
+# Assigment Operators
+x = 5 
+print(x)
+
+x += 3 
+# x = x + 3
+print(x)
+
+x -= 3
+# # x = x - 3
+print(x)
+
+x *= 3
+# # x = x * 3
+print(x)
+
+x /= 3
+# x = x / 3
+print(x)
